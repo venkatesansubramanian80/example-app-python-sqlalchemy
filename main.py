1b917c83485184338077f17af9350d9a3face3ab
venkatesansubramanian <venkatesangit80@outlook.com>
2023-03-01 08:02:44 -0600

    This will make app secure and functional

--- main.py
+++ main.py
@@ -7,97 +7,72 @@
 
 from math import floor
 import os
+import pandas as pd
 import random
 import uuid
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy_cockroachdb import run_transaction
-from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
 
 from models import Account
 
 # The code below inserts new accounts.
 
 
-def create_accounts(session, num):
-    """Create N new accounts with random account IDs and account balances.
-    """
-    print("Creating new accounts...")
-    new_accounts = []
-    while num > 0:
-        account_id = uuid.uuid4()
-        account_balance = floor(random.random()*1_000_000)
-        new_accounts.append(Account(id=account_id, balance=account_balance))
-        seen_account_ids.append(account_id)
-        print(f"Created new account with id {account_id} and balance {account_balance}.")
-        num = num - 1
-    session.add_all(new_accounts)
+def get_files(path):
+    files = []
+    with os.scandir(path) as entries:
+        for entry in entries:
+            if entry.is_file():
+                files.append(entry.name)
+    return files
 
+def read_file(path):
+    with open(path, 'r') as file:
+        content = file.read()
+    return content
 
-def transfer_funds_randomly(session, one, two):
-    """Transfer money between two accounts.
-    """
-    try:
-        source = session.query(Account).filter(Account.id == one).one()
-    except NoResultFound:
-        print("No result was found")
-    except MultipleResultsFound:
-        print("Multiple results were found")
-    dest = session.query(Account).filter(Account.id == two).first()
-    print(f"Random account balances:\nAccount {one}: {source.balance}\nAccount {two}: {dest.balance}")
+def replace_values(string, replacements):
+    for old_value, new_value in replacements.items():
+        string = string.replace(old_value, new_value)
+    return string
 
-    amount = floor(source.balance/2)
-    print(f"Transferring {amount} from account {one} to account {two}...")
-
-    # Check balance of the first account.
-    if source.balance < amount:
-        raise ValueError(f"Insufficient funds in account {one}")
-    source.balance -= amount
-    dest.balance += amount
-
-    print(f"Transfer complete.\nNew balances:\nAccount {one}: {source.balance}\nAccount {two}: {dest.balance}")
-
-
-def delete_accounts(session, num):
-    """Delete N existing accounts, at random.
-    """
-    print("Deleting existing accounts...")
-    delete_ids = []
-    while num > 0:
-        delete_id = random.choice(seen_account_ids)
-        delete_ids.append(delete_id)
-        seen_account_ids.remove(delete_id)
-        num = num - 1
-
-    accounts = session.query(Account).filter(Account.id.in_(delete_ids)).all()
-
-    for account in accounts:
-        print(f"Deleted account {account.id}.")
-        session.delete(account)
 
 
 if __name__ == '__main__':
     # For cockroach demo:
     # DATABASE_URL=postgresql://demo:<demo_password>@127.0.0.1:26257?sslmode=require
     # For CockroachCloud:
-    # DATABASE_URL=postgresql://<username>:<password>@<globalhost>:26257/<cluster_name>.defaultdb?sslmode=verify-full&sslrootcert=<certs_dir>/<ca.crt>
-    db_uri = os.environ['DATABASE_URL'].replace("postgresql://", "cockroachdb://")
+    user_name = os.environ.get('user_name')
+    pass_data = os.environ.get('password_value')
+    database_name = os.environ.get('url_value')
+    DATABASE_URL=f"cockroachdb://{user_name}:{pass_data}/{database_name}?sslmode=verify-full"
+    db_uri = DATABASE_URL
     try:
         engine = create_engine(db_uri, connect_args={"application_name":"docs_simplecrud_sqlalchemy"})
     except Exception as e:
         print("Failed to connect to database.")
         print(f"{e}")
 
-    seen_account_ids = []
+    df_data = pd.DataFrame()
+    files = get_files('tables')
+    replacements = {
+        'capsule': 'cp',
+        'carum': 'ca',
+        'cumen': 'cm',
+        'capgov': 'cg'
+    }
+    for single_file in files:
+        s_table_data = read_file(f'tables/{single_file}')
+        new_string = replace_values(s_table_data, replacements)
+        s_table_create = new_string.split(',',4)[4].replace('"','')
 
-    run_transaction(sessionmaker(bind=engine),
-                    lambda s: create_accounts(s, 100))
+        with engine.begin() as db:
+            db.execute(s_table_create)
 
-    from_id = random.choice(seen_account_ids)
-    to_id = random.choice([id for id in seen_account_ids if id != from_id])
-
-    run_transaction(sessionmaker(bind=engine),
-                    lambda s: transfer_funds_randomly(s, from_id, to_id))
-
-    run_transaction(sessionmaker(bind=engine), lambda s: delete_accounts(s, 5))
+    with engine.connect() as db:
+        df_data = pd.read_sql(text('show tables from public'), db)
+        for single_index, single_row in df_data.iterrows():
+            single_table = single_row['table_name']
+            print(single_table)
